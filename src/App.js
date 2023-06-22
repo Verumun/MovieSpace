84f0f439cd51ad7dcf1999b4299fe14324aafadc
Verumun <nevezekiel3@gmail.com>
2023-06-22 05:31:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
+import searchIcon from './search.svg'
+import MovieCard from "./Components/MovieCard/MovieCard";
 
-function App() {
+// a03db50a
+const apiUrl = 'http://www.omdbapi.com?apikey=a03db50a';
+
+// const movie1 = {
+//     "Title": "John Wick: Chapter 4",
+//     "Year": "2023",
+//     "imdbID": "tt10366206",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_SX300.jpg"
+// }
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${apiUrl}&s=${title}`)
+        const data = await response.json();
+
+        // console.log(data.Search)
+        setMovies(data.Search)
+    }
+
+    // console.log(movies)
+
+    // searchMovies('')
+    // This makes the search load faster
+    useEffect(() =>{
+        searchMovies(searchTerm)
+    })
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <h1>MovieSpace</h1>
+
+        <div className="search">
+            <input
+                placeholder="Search for movies"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        
+            <img 
+            src={searchIcon}
+            alt="search"
+            // onClick={() => searchMovies(searchTerm)}
+            />
+        </div>
+
+        {
+            movies?.length > 0 ? (
+                
+                <div className="container">
+                    {/* I created props */}
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+                    ))}
+                </div>
+
+            ) : (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+            )
+        }
+
     </div>
+    
   );
-}
+};
 
 export default App;
